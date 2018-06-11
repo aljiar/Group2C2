5d1f953b3e33378e91acd8e36668a6c4d04d1d74
Yeri Soruco Cabrera <yerisoruco@gmail.com>
2018-06-11 14:25:21 -0400

    change users and add Menu.cs

--- FinalProject/FinalProject/Models/User.cs
+++ FinalProject/FinalProject/Models/User.cs
@@ -8,10 +8,64 @@ namespace FinalProject
 {
     class User
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Name { get; set; }
-        public string LastName { get; set; }
-        public List<ShippingAddress> ShippingAddressesList { get; set; }
+        private string username;
+        private string password;
+        private string name;
+        private string lastname;
+
+       public List<ShippingAddress> ShippingAddressesList { get; set; }
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+
+            set
+            {
+                username = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+
+            set
+            {
+                lastname = value;
+            }
+        }
     }
 }
+
