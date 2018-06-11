54df2eef1391acf1bdaaa5d4a0e12d00090bda45
Elionor Cordova <elicg2012@hotmail.com>
2018-06-11 19:44:37 -0400

    develop: some bugs fixed

--- FinalProject/FinalProject/Models/Program.cs
+++ FinalProject/FinalProject/Models/Program.cs
@@ -1,4 +1,5 @@
-﻿using FinalProject.Services;
+﻿using FinalProject;
+using FinalProject.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,9 +25,9 @@ namespace FinalProject
             Product prod1 = new Product() { Code = "001", Name = "TV", Price = 10, Description = "Flat screen" };
             Product prod2 = new Product() { Code = "002", Name = "Radio", Price = 10, Description = "New radio" };
             Product prod3 = new Product() { Code = "003", Name = "Table", Price = 5, Description = "Table" };
-            ProductCart productCart1 = new ProductCart(prod1.Code, ShippingDeliveryType.Normal, null, 12);
-            ProductCart productCart2 = new ProductCart(prod2.Code, ShippingDeliveryType.Normal, null, 12);
-            ProductCart productCart3 = new ProductCart(prod3.Code, ShippingDeliveryType.Express, null, 1);
+            ProductCart productCart1 = new ProductCart(prod1.Code, ShippingDeliveryType1.Normal, null, 12);
+            ProductCart productCart2 = new ProductCart(prod2.Code, ShippingDeliveryType1.Normal, null, 12);
+            ProductCart productCart3 = new ProductCart(prod3.Code, ShippingDeliveryType1.Express, null, 1);
             //create
             manager.Create(productCart1); //success
             manager.Create(productCart2); //success
@@ -68,26 +69,26 @@ namespace FinalProject
             //read
             show<Store>(manager.Read());
             List<ShippingAddress> list = new List<ShippingAddress>();
-            UserManager manager = new UserManager();
+            UserManager manager2 = new UserManager();
             User user1 = new User("max", "pass", "Max", "Mendez", list);
             User user2 = new User("cam", "pass", "Camila", "Mendez", list);
             User user3 = new User("pedro", "pass", "Pedro", "Mendez", list);
             User user4 = new User("pedro", "pass", "Pedro", "Sanchez", list);
             //create
-            manager.Create(user1); //success
-            manager.Create(user2); //success
-            manager.Create(user3); //success
-            manager.Create(user4); //fail
+            manager2.Create(user1); //success
+            manager2.Create(user2); //success
+            manager2.Create(user3); //success
+            manager2.Create(user4); //fail
             //delete
-            manager.Delete("pedro"); //success
-            manager.Delete("laura"); //fail
+            manager2.Delete("pedro"); //success
+            manager2.Delete("laura"); //fail
             //update
             user2.Password = "newPass";
-            manager.Update("cam", user2); //success
-            manager.Update("cam", user1); //fail
-            manager.Update("kira", user4); //fail
+            manager2.Update("cam", user2); //success
+            manager2.Update("cam", user1); //fail
+            manager2.Update("kira", user4); //fail
             //read
-            show<User>(manager.Read());
+            show<User>(manager2.Read());
         }
 
         private static void show<T>(List<T> list)
