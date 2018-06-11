fb9d561a92f15b15d2f344038d677be772011076
Elionor Cordova <elicg2012@hotmail.com>
2018-06-11 14:31:41 -0400

    FE-001: CRUD actions added to the Product class.

--- FinalProject/FinalProject/Models/Program.cs
+++ FinalProject/FinalProject/Models/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FinalProject.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,33 @@ namespace FinalProject
     {
         static void Main(string[] args)
         {
+
+            testProduct();
+        }
+
+        static void testProduct()
+        {
+            ProductService prodServ = new ProductService();
+
+            Category categoria1 = new Category() { Name = "categoria1", Description = "descripcion de la primera categoria" };
+            Category categoria2 = new Category() { Name = "categoria2", Description = "descripcion de la segunda categoria" };
+
+            Product product1 = new Product() { Code = "1", Name = "Producto numero1", Price = 10.99, Category = categoria1, Description = "Descripcion del producto1", Type = Type1.Digital, ShippingDeliberyType = ShippingDeliveryType1.Express };
+            Product product2 = new Product() { Code = "2", Name = "Producto numero2", Price = 20.50, Category = categoria2, Description = "Descripcion del producto2", Type = Type1.Physical, ShippingDeliberyType = ShippingDeliveryType1.Free };
+            Product product3 = new Product() { Code = "3", Name = "Producto numero3", Price = 30.50, Category = categoria2, Description = "Descripcion del producto2", Type = Type1.Physical, ShippingDeliberyType = ShippingDeliveryType1.Free };
+
+            prodServ.Create(product1);
+            prodServ.Create(product2);
+            prodServ.Create(product1);
+            prodServ.Delete(product2.Code);
+            prodServ.Updata("1", product3);
+
+            foreach (Product p in prodServ.Read())
+            {
+                Console.WriteLine("---------------PRODUCTS IN THE DATA BASE------------\n");
+
+                p.showProducts();
+            }
         }
     }
 }
