c24520460aac950cc01f3208321ece29b28f2007
Isabella Defilippis <isastedef@yahoo.com>
2018-06-11 14:14:20 -0400

    FE-006: Merge conflict solved

--- FinalProject/FinalProject/Managers/ProductCartManager.cs
+++ FinalProject/FinalProject/Managers/ProductCartManager.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace FinalProject
 {
-<<<<<<< HEAD
     class ProductCartManager : IManager, ICRUD<ProductCart>
     {
         private Cart cart;
@@ -68,62 +67,6 @@ namespace FinalProject
         public List<ProductCart> Read()
         {
             return cart.ListProductCart;
-=======
-    class ProductCartManager
-    {
-        private DB myDB;
-
-        public ProductCartManager()
-        {
-            myDB = DB.Instance;
-        }
-
-        public bool checkIfProductCartExists(string productCode)
-        {
-            return myDB.ProductCarts.Exists((x => x.ProductCode == productCode));
-        }
-
-        private int getProductCartIndexByKey(string productCode)
-        {
-            return myDB.ProductCarts.FindIndex((x => x.ProductCode == productCode));
-        }
-
-        public bool create(ProductCart prodCart)
-        {
-            if (!checkIfProductCartExists(prodCart.ProductCode))
-            {
-                myDB.ProductCarts.Add(prodCart);
-                return true;
-            }
-            return false;
-        }
-
-        public bool delete(string key)
-        {
-            int index = getProductCartIndexByKey(key);
-            if (index != -1)
-            {
-                myDB.ProductCarts.RemoveAt(index);
-                return true;
-            }
-            return false;
-        }
-
-        public bool update(string key, ProductCart updatedProductCart)
-        {
-            int index = getProductCartIndexByKey(key);
-            if (index != -1 && key == updatedProductCart.ProductCode) //key must be the product code
-            {
-                myDB.ProductCarts[index] = updatedProductCart;
-                return true;
-            }
-            return false;
-        }
-
-        public List<ProductCart> read()
-        {
-            return myDB.ProductCarts;
->>>>>>> 3d7e367d886e7c354fdbade8c273d59ff4877931
         }
     }
 }
