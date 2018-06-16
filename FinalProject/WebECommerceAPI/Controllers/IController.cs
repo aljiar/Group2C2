94c5505a21fe967ec5f4978ef8aa970f933a1f2c
Elionor Cordova <elicg2012@hotmail.com>
2018-06-16 15:50:47 -0400

    API: Base of API model and the Interface with principal methods added.

---
+++ FinalProject/WebECommerceAPI/Controllers/IController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebECommerceAPI.Controllers
+{
+    interface IController
+    {
+        HttpResponseMessage GetInfo2();
+
+        HttpResponseMessage GetInfo(int id);
+
+        HttpResponseMessage PostInfo(Object content);
+
+        HttpResponseMessage UpdateInfo(Object res);
+
+        HttpResponseMessage DeleteInfo(Object id);
+    }
+}
