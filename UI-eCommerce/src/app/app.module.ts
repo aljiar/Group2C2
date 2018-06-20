6c7774a30fa630b451198ca56f0aca8e6b04e70e
Isabella Defilippis <isastedef@yahoo.com>
2018-06-20 12:23:52 -0400

    API-FIRST: Login functionality added

--- UI-eCommerce/src/app/app.module.ts
+++ UI-eCommerce/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
@@ -22,7 +23,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- UI-eCommerce/src/app/components/login-page/login-page.component.ts
+++ UI-eCommerce/src/app/components/login-page/login-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { Observable } from 'rxjs';
+import {Router} from '@angular/router'; 
 
 @Component({
   selector: 'app-login-page',
@@ -10,17 +11,25 @@ import { Observable } from 'rxjs';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor(private userService: UserService) { 
+  constructor(private userService: UserService, private router:Router) { 
 
   }
 
   ngOnInit() {
+    if (localStorage.getItem('username')) {
+      this.router.navigate([''])
+    }
   }
   
-  login(form:NgForm){
+  loginButtonPressed(form:NgForm){
     this.userService.getUserByUsername(form.value.username).subscribe(
-      data => console.log("Awesome"),
-      err => console.log("Error")
+      data => this.login(form.value.username),
+      err => alert("Username does not exist")
     );
+  }
+
+  login(username: string){
+    localStorage.setItem('username',username);
+    this.router.navigate([''])
   }
 }
