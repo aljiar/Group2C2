f4e0f4dfaba4a398afa02c6fd01f7a1ca04d7e49
Elionor Cordova <elicg2012@hotmail.com>
2018-06-20 19:14:24 -0400

    UI-THIRD: Functionality to the Third page added.

--- UI-eCommerce/src/app/app.module.ts
+++ UI-eCommerce/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { THIRDComponent } from './third/third.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    THIRDComponent
   ],
   imports: [
     BrowserModule,
